b63f40542732fe1161f66b45effa5c1d73c2788a
varad_dandagavha <77523268+varadbotdev@users.noreply.github.com>
2024-11-14 07:12:58 +0530

    Update ListBFS.c

--- pokemon/Pokemon/Graphs/ListBFS.c
+++ pokemon/Pokemon/Graphs/ListBFS.c
@@ -1,130 +1,144 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define MAX_VERTICES 100 // Maximum number of vertices
-
-// Define the structure for the adjacency list node
-struct Node {
-    int vertex;
-    struct Node* next;
+#include<stdio.h>
+#include<stdlib.h>
+int no_vertices;
+struct node
+{
+	int data;
+	struct node *next;
 };
+void readgraph(struct node *ad[]);
+void printgraph(struct node *ad[]);
+void bfs(struct node *ad[],int start,int visited[]);
+void dfs(struct node *ad[],int start,int visited[]);
+int main()
+{
+	int i,j,k,ch,start;
+	printf("Enter the total numbe of vertex :");
+	scanf("%d",&no_vertices);
+	int visited[no_vertices];
+	struct node *adj[no_vertices];
+	for(i=0;i<no_vertices;i++)
+	{
+		adj[i] = NULL;
+	}
+	readgraph(adj);
 
-// Define the structure for the graph
-struct Graph {
-    struct Node* adjList[MAX_VERTICES];
-    int vertices;
-};
+	do{
+		printf("\n Enter 1 for BFS\n Enter 2 for DFS\n Enter 3 to Print the adjacency list\nEnter 4 to Exit : ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1: 
+				printf("Enter the vertex from which do you wanted to start :");
+				scanf("%d",&start);
+				for(int k=0;k<no_vertices;k++)
+					visited[k]=0;
+				bfs(adj,start,visited); break;
+			
+			case 2: 
+				printf("Enter the vertex from which do you wanted to start :");
+				scanf("%d",&start);
+				for(int k=0;k<no_vertices;k++)
+					visited[k]=0;
 
-// Function to create a new adjacency list node
-struct Node* createNode(int vertex) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->vertex = vertex;
-    newNode->next = NULL;
-    return newNode;
+				printf("DFS : ");
+				dfs(adj,start,visited); 
+				break;
+
+			case 3: printgraph(adj);break;
+
+			case 4: break;
+		}
+	}while(ch!= 4);
+	
+
+
+
+	return 0;
 }
+void readgraph(struct node *ad[])
+{
+	struct node *newnode;
+	int i,j,k,data;
 
-// Function to initialize a graph
-void initializeGraph(struct Graph* graph, int vertices) {
-    graph->vertices = vertices;
-    for (int i = 0; i < vertices; i++) {
-        graph->adjList[i] = NULL; // Initialize each adjacency list as empty
-    }
+	for(i=0;i<no_vertices;i++)
+	{
+		struct node *last =NULL;
+		printf("\nEnter the Number of neighbours of %d  :",i);
+		scanf("%d",&k);
+
+		for(j=0;j<k;j++)
+		{
+			printf("Enter the value of %d neighbour of %d : ",j,i);
+			scanf("%d",&data);
+
+			newnode = (struct node*)malloc(sizeof(struct node*));
+			newnode->data = data;
+			newnode->next = NULL;
+			if(ad[i]==NULL)
+			{
+				ad[i] = newnode;
+			}
+			else
+				last->next = newnode;
+
+			last = newnode;
+		}
+	}
 }
-
-// Function to add an edge to the graph
-void addEdge(struct Graph* graph, int u, int v) {
-    // Add edge from u to v
-    struct Node* newNode = createNode(v);
-    newNode->next = graph->adjList[u];
-    graph->adjList[u] = newNode;
-
-    // For undirected graph, add edge from v to u
-    newNode = createNode(u);
-    newNode->next = graph->adjList[v];
-    graph->adjList[v] = newNode;
+void printgraph(struct node *ad[])
+{
+	struct node *ptr  = NULL;
+	int i,j;
+	for(i=0;i<no_vertices;i++)
+	{
+		ptr = ad[i];
+		printf("\n The neighbour of %d are :",i);
+		while(ptr != NULL)
+		{
+			printf("%d\t",ptr->data);
+			ptr = ptr->next;
+		}
+	}
 }
+void bfs(struct node *ad[],int start,int visited[])
+{
+	int queue[no_vertices],front=-1,rear=-1;
 
-// Function to perform BFS traversal
-void BFS(struct Graph* graph, int start) {
-    bool visited[MAX_VERTICES] = {false};
-    int queue[MAX_VERTICES];
-    int front = -1, rear = -1;
+	
+	printf("BFS : ");
+	queue[++rear] = start;
+	visited[start]=1;
+	front++;
+	while(front<=rear)
+	{
+		printf("%d\t",queue[front]);
+		struct node *ptr;
+		ptr =  ad[queue[front++]];
+		while(ptr != NULL)
+		{
+			if(visited[ptr->data]!=1)
+			{
+				queue[++rear] = ptr->data;
+				visited[ptr->data] =1;
+			}
+			ptr = ptr->next;
+		}
 
-    // Mark the start vertex as visited and enqueue it
-    visited[start] = true;
-    queue[++rear] = start;
-    
-    printf("BFS Traversal starting from vertex %d: ", start);
+	}
 
-    while (front != rear) {
-        // Dequeue a vertex from the queue
-        int currentVertex = queue[++front];
-
-        // Print the current vertex
-        printf("%d ", currentVertex);
-
-        // Traverse all adjacent vertices of the current vertex
-        struct Node* adjList = graph->adjList[currentVertex];
-        while (adjList != NULL) {
-            int adjVertex = adjList->vertex;
-            if (!visited[adjVertex]) {
-                visited[adjVertex] = true;  // Mark the vertex as visited
-                queue[++rear] = adjVertex;  // Enqueue the adjacent vertex
-            }
-            adjList = adjList->next;  // Move to the next adjacent vertex
-        }
-    }
-    printf("\n");
 }
+void dfs(struct node *ad[],int start, int visited[])
+{
 
-// Function to create the graph using user input
-void createGraph(struct Graph* graph, int vertices) {
-    int u, v;
-    printf("Enter the edges of the graph (u, v) where u and v are vertex indices (0 to %d):\n", vertices - 1);
-    printf("Enter -1 -1 to stop.\n");
-
-    while (1) {
-        printf("Enter edge (u v): ");
-        scanf("%d %d", &u, &v);
-
-        if (u == -1 && v == -1) {
-            break;
-        }
-
-        if (u >= 0 && u < vertices && v >= 0 && v < vertices) {
-            addEdge(graph, u, v);  // Add edge to the graph
-        } else {
-            printf("Invalid vertices! Please enter valid vertices within the range 0 to %d.\n", vertices - 1);
-        }
-    }
-}
-
-int main() {
-    int vertices;
-    struct Graph graph;
-
-    // Input the number of vertices
-    printf("Enter the number of vertices in the graph: ");
-    scanf("%d", &vertices);
-
-    if (vertices > MAX_VERTICES) {
-        printf("Exceeded maximum number of vertices!\n");
-        return -1;
-    }
-
-    // Initialize the graph
-    initializeGraph(&graph, vertices);
-
-    // Create the graph by taking user input
-    createGraph(&graph, vertices);
-
-    int startVertex;
-    printf("Enter the starting vertex for BFS: ");
-    scanf("%d", &startVertex);
-
-    // Perform BFS traversal from the start vertex
-    BFS(&graph, startVertex);
-
-    return 0;
+	visited[start] = 1;
+	printf("%d\t",start);
+	struct node *ptr;
+	ptr = ad[start];
+	while(ptr!=NULL)
+	{
+		if(visited[ptr->data]==0)
+			dfs(ad,ptr->data,visited);
+		ptr = ptr->next;
+	}
 }
